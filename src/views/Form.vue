5dedac9636fa9ef20f9d0497a39d4002e45afd49
tai5863 <taisws5863@gmail.com>
2020-04-09 22:47:39 +0900

    fixed form.vue

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -22,7 +22,7 @@
           </div>
           <div class="item_container">
             <h2 class="form_title">執筆者</h2>
-            <input type="text" class="input1" v-model="author" @keyup.enter="send()">
+            <input type="text" class="input1" v-model="author">
           </div> 
           <input class="send" value="送信する" @click="send()">
         </form>
